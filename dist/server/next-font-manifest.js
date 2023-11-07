1c52c1531b3dc3a94754c0e5088efa7bb79527ca
javierbarbozam <barboza.javier497@gmail.com>
2023-11-06 23:13:21 -0600

    dist folder first try

---
+++ dist/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/javierbarbozamunoz/Desktop/CETAV/Cursos/5-cuatri-2023/cortez-amarillo/cortez-amarillo/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"8r7OlzdUM/ydZvCwd78Yq73uf6zS2mWa8r89/XK97tA=\"}"
